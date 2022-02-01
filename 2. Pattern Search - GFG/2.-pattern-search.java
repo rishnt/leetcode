7033919bf2c9d47ea2447b8cf8cc9d5d6338d7e5
rishnt <94091947+rishnt@users.noreply.github.com>
2022-02-01 23:00:33 +0530

    Added solution - LeetHub

---
+++ 2. Pattern Search - GFG/2.-pattern-search.java
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = "", p = "";
+            s = sc.next();
+            p = sc.next();
+            
+            Solution obj = new Solution();
+            
+            if(obj.search(p, s) == true)
+              System.out.println("Yes");
+             else
+              System.out.println("No");
+            
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if the given pattern exists in the given string or not.
+    static boolean search(String pat, String txt)
+    {
+       int n=txt.length();
+       int m=pat.length();
+       for(int i=0;i<=n-m;){
+           int j;
+           for(j=0;j<m;j++){
+               if(pat.charAt(j)!=txt.charAt(i+j)){
+                   break;
+               }
+          }
+          if(j==m){
+              return true;
+          }
+          else if(j==0){
+              i++;
+          }
+          else{
+              i=i+j;
+          }
+           
+       }
+       return false;
+    }
+}
