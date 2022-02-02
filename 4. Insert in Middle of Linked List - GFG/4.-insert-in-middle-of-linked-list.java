7c1aace17152c7fa35e9e09c0a3839729e31a8c4
rishnt <94091947+rishnt@users.noreply.github.com>
2022-02-02 23:39:37 +0530

    Added solution - LeetHub

---
+++ 4. Insert in Middle of Linked List - GFG/4.-insert-in-middle-of-linked-list.java
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+   /*   int c=0;
+      Node curr=head;
+      while(curr!=null){
+          c++;
+          curr=curr.next;}
+          int pos=0;
+      /*if(c%2!=0){
+          pos=(c+1)/2;
+      }
+      else{
+          pos=c/2;
+      }
+        pos=(int)Math.ceil(c/2);
+    
+      Node temp=head;
+      for(int i=0;i<=pos-1;i++){
+          temp=temp.next;
+      }
+      Node t=new Node(data);
+      t.next=temp.next;
+      temp.next=t;
+      return head;*/
+      Node curr=head;
+       float len=0;
+       while(curr!=null){
+           len=len+1;
+           curr=curr.next;
+       }int mid=0;
+       
+           mid=(int)Math.ceil(len/2);
+           curr=head;
+           for(int i=1;i<=mid-1;i++){
+               curr=curr.next;
+           }Node t=new Node(data);
+           t.next=curr.next;
+           curr.next=t;
+           return head;
+    }
+}
