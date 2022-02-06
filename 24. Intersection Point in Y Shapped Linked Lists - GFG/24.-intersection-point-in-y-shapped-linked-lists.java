e802a46459033b067bc24b8340c558e3e02b40b8
rishnt <94091947+rishnt@users.noreply.github.com>
2022-02-07 00:32:55 +0530

    Added solution - LeetHub

--- 24. Intersection Point in Y Shapped Linked Lists - GFG/24.-intersection-point-in-y-shapped-linked-lists.java
+++ 24. Intersection Point in Y Shapped Linked Lists - GFG/24.-intersection-point-in-y-shapped-linked-lists.java
@@ -127,34 +127,38 @@ class Intersect
     //Function to find intersection point in Y shaped Linked Lists.
 	int intersectPoint(Node head1, Node head2)
 	{
-         Node ptr1= head1;
-         Node ptr2 = head2;
-         int count1 = 0, count2 = 0;
-         while(ptr1!=null){
-             count1++;
-             ptr1=ptr1.next;
-             
+        Node temp1=head1;
+        int c1=0,c2=0;
+        while(temp1!=null){
+            c1++;
+            temp1=temp1.next;
+        }
+        Node temp2=head2;
+        while(temp2!=null){
+            c2++;
+            temp2=temp2.next;
+        }
+        Node curr1=head1;
+        Node curr2=head2;
+        int dif=Math.abs(c1-c2);
+        if(c1>c2){
+            
+            for(int i=0;i<dif;i++){
+                curr1=curr1.next;
+            }
          }
-         while(ptr2!=null){
-             count2++;
-             ptr2 = ptr2.next;
+         else if(c2>c1){
+             for(int i=0;i<dif;i++){
+                 curr2=curr2.next;
+             }
          }
-         ptr1 = head1;
-         ptr2 = head2;
-         int diff = Math.abs(count1 - count2);
-         if(count1 > count2){
-             for(int i = 0; i < diff;i++) ptr1 = ptr1.next;
-         }
-         else if(count2 > count1){
-             for(int i = 0; i < diff; i++) ptr2 = ptr2.next;
-         }
-         while(ptr1!=ptr2){
-             ptr1 = ptr1.next;
-             ptr2 = ptr2.next;
-         }
-         if(ptr1 !=null)
-         return ptr1.data;
-         return -1; 
+         while(curr1!=curr2){
+             curr1=curr1.next;
+             curr2=curr2.next;
+         }if(curr1!=null){
+         return curr1.data;}
+         return -1;
+         
 	}
 }
 
