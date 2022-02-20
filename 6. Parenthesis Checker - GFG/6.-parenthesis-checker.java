639c16e4e1f010929fa931b27b3406f4bfa869ad
rishnt <94091947+rishnt@users.noreply.github.com>
2022-02-20 10:46:56 +0530

    Added solution - LeetHub

---
+++ 6. Parenthesis Checker - GFG/6.-parenthesis-checker.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<x.length();i++){
+            char ch=x.charAt(i);
+            
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;}
+                
+                 if(matching(st.peek(),ch)==false){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        
+        }
+        return st.isEmpty();
+    }
+    static boolean matching(char a,char b){
+        return(a=='('&&b==')'||a=='{'&&b=='}'||a=='['&&b==']');
+    }
+}
