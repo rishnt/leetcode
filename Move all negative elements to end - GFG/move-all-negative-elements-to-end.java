6494649d12a234e155c064c2d0bf570dd9d1e0bb
rishnt <94091947+rishnt@users.noreply.github.com>
2022-02-25 21:10:20 +0530

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+            
+            System.out.println();
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+      int[] ar=new int[n];
+      int size=0;
+      for(int i=0;i<n;i++){
+          if(arr[i]>0){
+              ar[size]=arr[i];
+              size++;
+          }
+      }
+      for(int j=0;j<n;j++){
+          if(arr[j]<0){
+              ar[size]=arr[j];
+              size++;
+          }
+      }
+      for(int i=0;i<n;i++){
+          arr[i]=ar[i];
+      }
+    }
+}
