bb1e03c64909740e1e9f066ef1ffd6b34f1f097d
rishnt <94091947+rishnt@users.noreply.github.com>
2022-03-03 18:31:14 +0530

    Added solution - LeetHub

--- Find duplicates in an array - GFG/find-duplicates-in-an-array.java
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.java
@@ -22,52 +22,21 @@ class GFG {
 
 class Solution {
     public static ArrayList<Integer> duplicates(int arr[], int n) {
-       /*ArrayList<Integer> element=new ArrayList<Integer>();
-      int[] temp=new int[n];
-       boolean p;
-       int tem;
-       for(int i=0;i<n;i++){
-           temp[i]=-1;
-       }
-       for(int i=0;i<n;i++){
-         /*  if(temp[arr[i]]){
-               p=arr[i];
-               element.add(p);
-           }
-           temp[arr[i]]=true;*///temp[arr[i]]=++temp[arr[i]];
-           /*p=temp[arr[i]];
-           if(p==false){
-              temp[arr[i]]=true; 
-           }
-           else{
-               tem=arr[i];
-               element.add(tem);
-           }*/
-           
-       //}
-       /*for(int i=0;i<n;i++){
-           if(temp[i]>0)
-           element.add(i);
-       }
-        return element;*/
-         ArrayList<Integer> a1 = new ArrayList<>();
-       int[] d = new int[n];
+        ArrayList<Integer> list=new ArrayList<>();
+        int[] temp=new int[n];
+        for(int i:arr){
+            temp[i]++;
+        }
+        for(int i=0;i<n;i++){
+            if(temp[i]>1)
+                list.add(i);
+            
+        }
+        if(list.isEmpty())
+            list.add(-1);
+        
        
-       for(int i=0;i<n;i++){
-           d[i] = -1;
-       
-}
-       for(int i=0;i<n;i++){
-           d[arr[i]] = ++d[arr[i]];
-       }
-
-       for(int i=0;i<n;i++){
-           if(d[i]>0)
-               a1.add(i);}
-      
-       if(a1.isEmpty())
-           a1.add(-1);
-           
-           return a1;
+        return list;
+         
     }
 }
