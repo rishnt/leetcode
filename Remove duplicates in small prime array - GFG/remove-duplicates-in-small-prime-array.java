ca876f5ebc603d8408f53b8a1ed0361c59d5e341
rishnt <94091947+rishnt@users.noreply.github.com>
2022-03-19 11:48:15 +0530

    Added solution - LeetHub

---
+++ Remove duplicates in small prime array - GFG/remove-duplicates-in-small-prime-array.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+import java.util.*;
+import java.util.Scanner;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0; i<n; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            Solution g = new Solution();
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            result = g.removeDuplicate(arr,n);
+            
+            for(int i=0;i<result.size();i++)
+                System.out.print(result.get(i) + " ");
+            System.out.println();
+        t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    ArrayList<Integer> removeDuplicate(int arr[], int n)
+    {
+      ArrayList<Integer> list=new ArrayList<Integer>();
+      list.add(arr[0]);
+      for(int i=1;i<n;i++){
+          if(list.contains(arr[i])){
+              continue;
+          }
+          else{
+              list.add(arr[i]);
+          }
+      }
+      return list;
+    }
+}
