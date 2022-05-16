0b57e3e1479b12ec6b2f53c43fb016944c422235
rishnt <94091947+rishnt@users.noreply.github.com>
2022-05-16 12:25:39 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.java
+++ Binary Search - GFG/binary-search.java
@@ -1,50 +1,49 @@
 // { Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-public class GFG {
-    public static void main (String[] args) throws IOException {
+  public class GFG {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        while(T>0)
-        {
+        while (T > 0) {
             int n = sc.nextInt();
             int arr[] = new int[n];
-            for(int i=0;i<n;i++)
-            {
-                arr[i]=sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
             }
-             
-            int key =sc.nextInt();
+
+            int key = sc.nextInt();
             Solution g = new Solution();
-            System.out.println(g.binarysearch(arr,n,key));
+            System.out.println(g.binarysearch(arr, n, key));
             T--;
         }
     }
 }
-
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 class Solution {
-    int binarysearch(int arr[], int n, int k){
-        int low=0,high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]<k){
-                low=mid+1;
-            }
-            else if(arr[mid]>k){
-                high=mid-1;
-            }
-            else if(arr[mid]==k){
-                return mid;
-            }
-        }
-        return -1;
+    int binarysearch(int arr[], int n, int k) {
+          int low=0;
+          int high=n-1;
+          while(low<=high){
+              int mid=(low+high)/2;
+              
+              if(arr[mid]==k){
+                  return mid;
+              }
+              else if(arr[mid]<k){
+                  low=mid+1;
+              }
+              else if(arr[mid]>k){
+                  high=mid-1;
+              }
+          }
+          return -1;
     }
 }
