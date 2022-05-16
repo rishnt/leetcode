bb385224af1661497c05e44801e10d752cb6c4a2
rishnt <94091947+rishnt@users.noreply.github.com>
2022-05-16 11:50:06 +0530

    Added solution - LeetHub

--- Wave Array - GFG/wave-array.java
+++ Wave Array - GFG/wave-array.java
@@ -2,64 +2,81 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
     
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-		
-		while(t-->0)
-		{
-		    int n = Integer.parseInt(br.readLine().trim());// taking size of array
-		    int arr[] = new int[n]; // declaring array of size n
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    for(int i=0; i<n; i++){
-		        arr[i]=Integer.parseInt(inputLine[i]); // input elements of array
-		    }
-		    
-		    Solution obj = new Solution();
-		    
-		    
-		    obj.convertToWave(arr, n);
-		    
-		    StringBuffer sb = new StringBuffer(); 
-            for (int i = 0; i < n; i++) 
-                sb.append(arr[i] + " "); 
-                
-		    System.out.println(sb); // print array
-		}
-	}
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
 }
 
-
- // } Driver Code Ends
-class Solution{
-
-    
-    // arr: input array
-    // n: size of the array
-    //Function to sort the array into a wave-like array.
-    public static void convertToWave(int arr[], int n){
-        int temp=0;
-        if(n%2==0){
-        for(int i=0;i<n;i+=2){
-         temp=arr[i];
-         arr[i]=arr[i+1];
-         arr[i+1]=temp;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n; 
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] a = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            obj.convertToWave(n, a);
+            StringBuffer sb=new StringBuffer("");
+            for(int i : a){
+                sb.append(i+" ");
+            }
+            System.out.println(sb.toString());
         }
-        }
-        else{
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+       
+       int temp=0;
+       if(n%2==0){
+       for(int i=0;i<n;i+=2){
+           temp=a[i];
+           a[i]=a[i+1];
+           a[i+1]=temp;
+       }
+       }
+       else{
             for(int i=0;i<n-1;i+=2){
-         temp=arr[i];
-         arr[i]=arr[i+1];
-         arr[i+1]=temp;
-        }
-        }
-        
+           temp=a[i];
+           a[i]=a[i+1];
+           a[i+1]=temp;
+       }
+       
+       
+       }
     }
     
 }
-
-
-// { Driver Code Starts.
-  // } Driver Code Ends
+        
