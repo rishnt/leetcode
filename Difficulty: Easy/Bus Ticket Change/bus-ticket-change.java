3f26e66814a35762e3c9c62307386d9afb6dc7b4
Rishant Mishra <94091947+rishnt@users.noreply.github.com>
2025-09-16 23:07:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
+++ Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
@@ -1,28 +1,32 @@
 class Solution {
     public boolean canServe(int[] arr) {
-     int five = 0, ten = 0; // track available 5s and 10s
-
-    for (int bill : arr) {
-        if (bill == 5) {
-            five++;
-        } else if (bill == 10) {
-            if (five > 0) {
-                five--;
-                ten++;
-            } else {
-                return false; // no 5 to give as change
-            }
-        } else { // bill == 20
-            if (ten > 0 && five > 0) {
-                ten--;
-                five--;
-            } else if (five >= 3) {
-                five -= 3;
-            } else {
-                return false; // not enough change
-            }
-        }
-    }
-    return true;
+   int five =0,ten=0;
+   for(int i=0;i<arr.length;i++){
+       if(arr[i]==5){
+           five++;
+       }
+       else if(arr[i]==10){
+           if(five>0){
+               five--;
+               ten++;
+           }
+           else{
+               return false;
+           }
+       }
+       else{
+           if(ten>0 && five>0){
+               ten--;
+               five--;
+           }
+           else if(five>=3){
+             five-=3;
+           }
+           else{
+               return false;
+           }
+       }
+   }
+   return true;
     }
 }
