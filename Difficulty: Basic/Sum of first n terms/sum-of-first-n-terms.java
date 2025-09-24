b751849a1bc5cbaebdd792dc9192300f8fe729b5
Rishant Mishra <94091947+rishnt@users.noreply.github.com>
2025-09-24 21:55:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+
+class Solution {
+    int sumOfSeries(int n) {
+      int sum = (n * (n + 1)) / 2;  // sum of first n numbers
+    return sum * sum;  
+    }
+}
