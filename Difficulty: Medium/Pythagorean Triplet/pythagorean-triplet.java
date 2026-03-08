64a1f00900918aa1ec6d7a7d9011cf9a89dccf45
Rishant Mishra <94091947+rishnt@users.noreply.github.com>
2026-03-08 11:11:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
@@ -1,18 +1,17 @@
 class Solution {
     boolean pythagoreanTriplet(int[] arr) {
-      HashSet<Integer> h=new HashSet<Integer>();
-      
-      for(int i=0;i<arr.length;i++){
-          h.add(arr[i]*arr[i]);
-      }
-      
-      for(int i=0;i<arr.length-1;i++){
-          for(int j=i+1;j<arr.length;j++){
-              if(h.contains(arr[i]*arr[i]+arr[j]*arr[j])){
-                  return true;
-              }
-          }
-      }
-      return false;
+        HashSet<Integer> h = new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++){
+            h.add(arr[i]*arr[i]);
+        }
+        
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(h.contains(arr[i]*arr[i]+arr[j]*arr[j])){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
