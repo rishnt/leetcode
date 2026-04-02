5565a98df85ca7f73871b2d674c8a0616876fa76
Rishant Mishra <94091947+rishnt@users.noreply.github.com>
2026-04-02 09:57:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.java
@@ -0,0 +1,26 @@
+class Solution {
+    int countWays(int n, int k) {
+        // code here.
+          int ans = solve(n, k);
+        return ans;
+    }
+      int solve(int n, int k) {
+        int prev2 = k;
+        int prev1 = k * k;
+        int curr;
+        
+        if(n == 1)
+            return prev2;
+        if(n == 2)
+            return prev1;
+        
+        for(int i = 3; i <= n; i++) {
+            int ans = (prev1 + prev2)*(k - 1);
+            curr = ans;
+            // Shift
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
