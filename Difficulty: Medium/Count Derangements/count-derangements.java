a10a1079f2510028d12b1336597b332ebb3b81fe
Rishant Mishra <94091947+rishnt@users.noreply.github.com>
2026-04-20 01:30:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Derangements/count-derangements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int derangeCount(int n) {
+        // code here
+      return d[n];
+    }
+      static int[] d = new int[13];
+    static {
+        d[0] = 1;
+        d[1] = 0;
+        
+        for(int i = 2; i <= 12; i++) {
+            d[i] = (i - 1) * ((d[i - 1] + d[i - 2]));
+        }
+    }
+};
